29053cc6b23321d2670b24e06b561342d8f1878c
João Pedro Malta <pedromaltapedrozo@gmail.com>
2020-04-22 17:37:53 -0300

    melhoria na tela de cadastro e melhoria na funcionalidade em resolucoes menores

--- Script.js
+++ Script.js
@@ -1,3 +1,9 @@
 function proximoCadastro() {
     document.getElementById("cadastroP1").style.display = "none";
+    document.getElementById("cadastroP2").style.display = "flex";
+}
+
+function cadastroAnterior() {
+    document.getElementById("cadastroP1").style.display = "flex";
+    document.getElementById("cadastroP2").style.display = "none";
 }
